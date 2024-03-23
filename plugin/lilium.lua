7916148f946ccdf776b05d08c9dde8c92433bc49
Daniel Leong <me@dhleong.net>
2024-03-23 15:09:42 -0400

    Fix: improve null-ls detection with LazyVim
    LazyVim seems to intercept the null-ls require, so instead we pcall
    against null-ls.helpers to check if it's available.

--- plugin/lilium.lua
+++ plugin/lilium.lua
@@ -1,7 +1,7 @@
 if vim.fn.has('nvim') then
-  local ok, null_ls = pcall(require, 'null-ls')
+  local ok, _ = pcall(require, 'null-ls.helpers')
   if ok and vim.g.lilium_enable_source ~= 0 then
-    null_ls.enable(require 'lilium.source')
+    require'null-ls'.enable(require 'lilium.source')
   end
 
   require 'lilium'.setup_common()
