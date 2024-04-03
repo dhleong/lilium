151286664bee97ae8fc98bcdca4a6c0a7f3b19db
Daniel Leong <me@dhleong.net>
2024-04-03 13:56:19 -0400

    Don't eagerly init in lazy land; add a GBrowse handler
    rhubarb's handler seems to frequently fail for my use cases

--- lua/lilium/init.lua
+++ lua/lilium/init.lua
@@ -1,29 +1,64 @@
-local M = {}
+local M = {
+  _installed_browse_handler = nil,
+}
 
-local can_complete, completer = pcall(require, 'lilium.completer')
+local can_complete, completer = pcall(require, "lilium.completer")
 M.completer = completer
 
 function M._trigger_completion()
-  local ok, cmp = pcall(require, 'cmp')
+  local ok, cmp = pcall(require, "cmp")
   if ok then
     cmp.complete()
   end
 end
 
 function M.maybe_init_completion()
-  if can_complete and require 'null-ls'.is_registered('lilium') then
-    vim.cmd [[
+  if can_complete and require("null-ls").is_registered("lilium") then
+    vim.cmd([[
       inoremap <buffer> # #<cmd>lua require'lilium'._trigger_completion()<cr>
       inoremap <buffer> @ @<cmd>lua require'lilium'._trigger_completion()<cr>
-    ]]
+    ]])
   end
 end
 
 function M.setup_common()
-  if vim.api.nvim_create_user_command and require 'lilium.info'.is_available() then
-    vim.api.nvim_create_user_command('LiliumInfo', function()
-      require 'lilium.info'.open()
+  if vim.api.nvim_create_user_command and require("lilium.info").is_available() then
+    vim.api.nvim_create_user_command("LiliumInfo", function()
+      require("lilium.info").open()
     end, {})
+  end
+end
+
+---@class LiliumConfig
+local DEFAULT_CONFIG = {
+  install_fugitive_browse = true,
+}
+
+---@param config LiliumConfig
+function M.setup(config)
+  config = vim.tbl_deep_extend("force", DEFAULT_CONFIG, config)
+  M.setup_common()
+
+  if config.install_fugitive_browse then
+    local f = require("lilium.browse").handle_browse
+    M._installed_browse_handler = f
+
+    local handlers = (vim.g.fugitive_browse_handlers or {})
+    handlers[#handlers + 1] = f
+    vim.g.fugitive_browse_handlers = handlers
+  end
+end
+
+function M.deactivate()
+  if M._installed_browse_handler then
+    local handlers = (vim.g.fugitive_browse_handlers or {})
+    for i, f in ipairs(handlers) do
+      if f == M._installed_browse_handler then
+        table.remove(handlers, i)
+        vim.g.fugitive_browse_handlers = handlers
+        break
+      end
+    end
   end
 end
 
--- plugin/lilium.lua
+++ plugin/lilium.lua
@@ -1,8 +1,11 @@
-if vim.fn.has('nvim') then
-  local ok, _ = pcall(require, 'null-ls.helpers')
+if vim.fn.has("nvim") then
+  local ok, _ = pcall(require, "null-ls.helpers")
   if ok and vim.g.lilium_enable_source ~= 0 then
-    require'null-ls'.enable(require 'lilium.source')
+    require("null-ls").enable(require("lilium.source"))
   end
 
-  require 'lilium'.setup_common()
+  local is_lazy, _ = pcall(require, "lazy")
+  if not is_lazy then
+    require("lilium").setup_common()
+  end
 end
