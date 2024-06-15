b25ff0387126833c7cdff34a646326ea069413fb
Daniel Leong <me@dhleong.net>
2024-06-15 19:26:08 -0400

    Fix: resolve the enhanced_editor path to handle weird FS stuff on macs
    EG: we get a path like `/var/folders/...` but then `expand('%:p')` gives
    us something like `/private/var/folders/...`, so we can't find the
    prepared project config. Calling `resolve()` on the path we get gives us
    the path returned by `expand()`!

--- lua/lilium/lsp/enhanced_editor.lua
+++ lua/lilium/lsp/enhanced_editor.lua
@@ -12,7 +12,11 @@ end
 
 ---@param config {path: string, project: {cwd: string}}
 function M.prepare(config)
-  M._paths[config.path] = config.project
+  -- NOTE: config.path might not be the canonical path (esp on macOS)
+  -- which would prevent us from looking in the right place when calling
+  -- `get_project_path_for_file`. Using resolve() seems to fix this.
+  local path = vim.fn.resolve(config.path)
+  M._paths[path] = config.project
 end
 
 return M
